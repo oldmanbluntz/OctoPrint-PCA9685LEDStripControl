303829015945da8cc37740e99532394497016b22
Mike Bailey <michael.l.bailey@hotmail.com>
2023-04-06 10:54:42 -0400

    Update __init__.py
    Updated code to support newer CircuitPython support for the PCA9685 running through the FT232H on a desktop PC running Octoprint.

--- octoprint_PCA9685LEDStripControl/__init__.py
+++ octoprint_PCA9685LEDStripControl/__init__.py
@@ -1,50 +1,38 @@
-#
-# Copyright 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# coding=utf-8
-
 from __future__ import absolute_import
 from __future__ import division
 import time
 import re
-
 import octoprint.plugin
-import Adafruit_PCA9685
+import board
+from adafruit_pca9685 import PCA9685
 
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(120)
+i2c = board.I2C()
+pca = PCA9685(i2c)
+pca.frequency = 120
+
 
 class PCA9685LED:
-	pwm = None
-	pin = None
-	def __init__(self, pwm, pin):
-		self.pwm = pwm
-		self.pin = pin
+	def __init__(self, outpin):
+		self.pca = pca
+		self.outpin = outpin
+		self.duty_cycle = 0
 
-	def ChangeDutyCycle(self, duty_cycle):
-		self.pwm.set_pwm(self.pin, 0, int(duty_cycle))
+	def changedutycycle(self, duty_cycle):
+		self.duty_cycle = int(duty_cycle * 65535)
+		self.duty_cycle = max(0, min(self.duty_cycle, 65535))  # Clamp the duty_cycle value
+		self.pca.channels[self.outpin].duty_cycle = self.duty_cycle
 
 	def stop(self):
-		self.ChangeDutyCycle(0)
+		self.changedutycycle(0)
 
 
-class PCA9685LEDStripControlPlugin(octoprint.plugin.AssetPlugin,
-							octoprint.plugin.SettingsPlugin,
-							octoprint.plugin.ShutdownPlugin,
-							octoprint.plugin.StartupPlugin,
-							octoprint.plugin.TemplatePlugin):
+class PCA9685LEDStripControlPlugin(
+	octoprint.plugin.StartupPlugin,
+	octoprint.plugin.TemplatePlugin,
+	octoprint.plugin.SettingsPlugin,
+	octoprint.plugin.RestartNeedingPlugin,
+	octoprint.plugin.ShutdownPlugin
+):
 
 	def __init__(self):
 		self._leds = dict(r=None, g=None, b=None, w=None)
@@ -53,16 +41,16 @@ class PCA9685LEDStripControlPlugin(octoprint.plugin.AssetPlugin,
 		self._logger.debug(u"_unregister_leds()")
 		for i in ('r', 'g', 'b', 'w'):
 			if self._leds[i]:
-				self._leds[i].ChangeDutyCycle(0)
+				self._leds[i].changedutycycle(0)
 				self._leds[i].stop()
 		self._leds = dict(r=None, g=None, b=None)
 
 	def _register_leds(self):
 		self._logger.debug(u"_register_leds()")
 		for i in ('r', 'g', 'b', 'w'):
-			pin = self._settings.get_int([i])
-			self._logger.debug(u"got pin(%s)" % (pin,))
-			self._leds[i] = PCA9685LED(pwm, pin)
+			outpin = self._settings.get_int([i])
+			self._logger.debug(u"got pin(%s)" % (outpin,))
+			self._leds[i] = PCA9685LED(outpin)
 
 	def on_after_startup(self):
 		self._logger.debug(u"PCA9685LEDStripControl Startup")
@@ -71,33 +59,30 @@ class PCA9685LEDStripControlPlugin(octoprint.plugin.AssetPlugin,
 		self._logger.debug(u"PCA9685LEDStripControl Shutdown")
 		self._unregister_leds()
 
-	def HandleM150(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+	def handlem150(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
 		if gcode and cmd.startswith("M150"):
 			self._logger.debug(u"M150 Detected: %s" % (cmd,))
-			# Emulating Marlin 1.1.0's syntax
-			# https://github.com/MarlinFirmware/Marlin/blob/RC/Marlin/Marlin_main.cpp#L6133
-			dutycycles = {'r':0.0, 'g':0.0, 'b':0.0, 'w':0.0}
+			dutycycles = {'r': 0.0, 'g': 0.0, 'b': 0.0, 'w': 0.0}
 			for match in re.finditer(r'([RGUBWrgubw]) *(\d*)', cmd):
 				k = match.group(1).lower()
 				# Marlin uses RUB instead of RGB
-				if k == 'u': k = 'g'
+				if k == 'u':
+					k = 'g'
 				try:
 					v = float(match.group(2))
 				except ValueError:
 					# more than likely match.group(2) was unspecified
 					v = 255.0
-				v = v/255.0 * 4095.0 # convert RGB to RPi dutycycle
-				v = max(min(v, 4095.0), 0.0) # clamp the value
+				v = v / 255.0 * 65535.0  # convert RGB to RPi dutycycle
+				v = max(min(v, 65535.0), 0.0)  # clamp the value
 				dutycycles[k] = v
 				self._logger.debug(u"match 1: %s 2: %s" % (k, v))
 
-			for l in dutycycles.keys():
-				if self._leds[l]:
-					self._leds[l].ChangeDutyCycle(dutycycles[l])
+			for light in dutycycles.keys():
+				if self._leds[light]:
+					self._leds[light].changedutycycle(dutycycles[light])
 
 			return None,
-
-	##~~ SettingsPlugin mixin
 
 	def get_settings_version(self):
 		return 2
@@ -120,8 +105,9 @@ class PCA9685LEDStripControlPlugin(octoprint.plugin.AssetPlugin,
 		self._unregister_leds()
 		# cast to proper types before saving
 		for k in ('r', 'g', 'b', 'w'):
-			if data.get(k): data[k] = max(0, int(data[k]))
-		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+			if data.get(k):
+				data[k] = max(0, int(data[k]))
+		super().on_settings_save(data)
 		self._register_leds()
 
 	def on_settings_migrate(self, target, current=None):
@@ -131,26 +117,26 @@ class PCA9685LEDStripControlPlugin(octoprint.plugin.AssetPlugin,
 			self._settings.set(['w'], self.get_settings_defaults()["w"])
 			self._settings.set(['on_startup'], self.get_settings_defaults()["on_startup"])
 
-	##~~ Softwareupdate hook
-
 	def get_update_information(self):
-		return dict(
-			PCA9685LEDStripControl=dict(
-				displayName="PCA9685 LED Strip Control Plugin",
-				displayVersion=self._plugin_version,
+		return {
+			"PCA9685LEDStripControl": {
+				"displayName": "PCA9685LEDStripControl Plugin",
+				"displayVersion": self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="ozgunawesome",
-				repo="OctoPrint-PCA9685LEDStripControl",
-				current=self._plugin_version,
+				"type": "github_release",
+				"user": "OldMaNBlunTZ",
+				"repo": "OctoPrint-Pca9685ledcontrol",
+				"current": self._plugin_version,
 
-				# update method: pip
-				pip="https://github.com/ozgunawesome/OctoPrint-PCA9685LEDStripControl/archive/{target_version}.zip"
-			)
-		)
+				"pip": "https://github.com/OldMaNBlunTZ/OctoPrint-Pca9685ledcontrol/archive/{target_version}.zip",
+			}
+		}
 
-__plugin_name__ = "PCA9685 LED Strip Control"
+
+__plugin_name__ = "Pca9685ledstripcontrol Plugin"
+
+__plugin_pythoncompat__ = ">=3,<4"
+
 
 def __plugin_load__():
 	global __plugin_implementation__
@@ -159,6 +145,5 @@ def __plugin_load__():
 	global __plugin_hooks__
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
-		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.HandleM150
+		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.handlem150
 	}
-
